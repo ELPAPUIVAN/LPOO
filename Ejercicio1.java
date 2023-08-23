85e7cff6b2f0d4f5b08e7000a35ca69fe9160114
PAPU <142941617+ELPAPUIVAN@users.noreply.github.com>
2023-08-23 08:46:01 -0300

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,24 @@
+
+public class LosSanguchesDeMiga {
+
+	public static void main(String [] args)
+	{int u;
+		for(int i=1;i<101;i++) {
+			u=i%2;
+			if (u==0) {
+				System.out.print("\nel numero es par\n" + i);
+			}else {
+				System.out.print("\nel numero es impar\n" + i);
+			}
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+}
