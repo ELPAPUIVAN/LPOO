2796318ca6b696c5d99d86b9201e95de6e47a502
PAPU <142941617+ELPAPUIVAN@users.noreply.github.com>
2023-11-03 19:38:46 -0300

    Create ejercicio5.java

---
+++ ejercicio5.java
@@ -0,0 +1,17 @@
+
+package ejercicio;
+
+public class ejercicio5 {
+            public static void main(String[] args){
+                
+                int[] numeros ={1,2,3,4,5};
+                //declaro un array y sus respectivos valores
+                for (int numero : numeros) {
+                    //inicio un ciclo de iteracion for each que recorre todo un array
+                    System.out.println(numero);
+                    //muestro por consola el valor actual del array
+                }
+                
+                
+            }
+}
