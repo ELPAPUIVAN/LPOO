f37ac666fd375c04051bb00344307db3749c0ca0
PAPU <142941617+ELPAPUIVAN@users.noreply.github.com>
2023-11-03 19:36:18 -0300

    Create ejercicio1.java

---
+++ ejercicio1.java
@@ -0,0 +1,16 @@
+
+package ejercicio;
+
+
+public class ejercicio1 {
+       public static void main(String[] args){
+           for (int i = 0; i <= 100; i++) {
+               if(i%2==0){
+                   System.out.println(i+" es par"); 
+               }else{
+                   System.out.println(i+" no es par");
+               }
+           }
+           
+       }
+}
